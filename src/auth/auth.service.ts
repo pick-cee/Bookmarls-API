0e7c810575f2d5b6474d890b672467422edce995
pick-cee <akinloluwaolumuyide@gmail.com>
2023-07-04 12:35:04 +0100

    did the basic auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,45 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2'
+import { Prisma } from "@prisma/client";
 
 @Injectable()
 export class AuthService{
     constructor(private prisma: PrismaService) {}
     
-    signin(){
-        return { message: "I have signed in"}
+    async signin(dto: AuthDto){
+        const user = await this.prisma.user.findUnique({where: { email: dto.email}})
+        if(!user){
+          throw new ForbiddenException("Incorrect credentials")
+        }
+        const pwMatches = await argon.verify(user.hash, dto.password)
+        if(!pwMatches){
+          throw new ForbiddenException("Incorrect credentials")
+        }
+        delete user.hash
+        return user
     }
-    signup(){
-        return { message: "I have signed up"}
-    }
+    async signup(dto: AuthDto) {
+        // generate the password hash
+        const hash = await argon.hash(dto.password);
+        // save the new user in the db
+        try {
+          const user = await this.prisma.user.create({
+            data: {
+              email: dto.email,
+              hash,
+            },
+          });
+          delete user.hash;
+          return user
+        } catch (error) {
+          if ( error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2002') {
+              throw new ForbiddenException('Credentials taken');
+            }
+          }
+          throw error;
+        }
+      }
 }
